78d501a0fbf2afff1fe659a10abe4eb8bf6bb350
shudhanshuShekhar <shudhanshuraz01@gmail.com>
2023-08-10 15:14:17 +0530

    add

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const cors = require("cors")
+server.use(cors())
 
 server.use(middlewares);
 server.use(router);
